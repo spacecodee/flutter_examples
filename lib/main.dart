c055420091238d964466486af79e229b7b9e5498
Space Codee <spacecodee@gmail.com>
2022-02-14 22:26:09 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_examples/src/widgets/finance_dark_mode/finance_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColorDark: const Color(0xff2a2a2a),
+        primarySwatch: Colors.blue,
+      ),
+      home: const FinanceApp(),
+    );
+  }
+}
